b634d7f4486c118edba7ca54232e75e42b5aec83
heimedina <heidimedina23@gmail.com>
2025-03-18 18:10:26 +0100

    Actualizo nuevo ejercicio

---
+++ Ejercicios_logica/src/calculo/peso/CalculoPeso.java
@@ -0,0 +1,15 @@
+package calculo.peso;
+
+import java.util.Scanner;
+
+public class CalculoPeso {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Introduzca el valor del peso:");
+		double peso = Double.parseDouble(keyboard.nextLine()); //Double para admitir pesos con decimales
+		double pesoLUNA = peso*17/100 ; // 17% = 17/100 
+		System.out.println("Un peso de "+ peso+" Kilos en la Tierra equivalen a "+pesoLUNA+" Kilos en la Luna");
+	}
+
+}
