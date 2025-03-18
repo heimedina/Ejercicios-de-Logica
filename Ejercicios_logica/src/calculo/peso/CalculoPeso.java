16d44462c85a00fa11cee92fdb630e8a6ff0ca65
heimedina <heidimedina23@gmail.com>
2025-03-18 18:14:51 +0100

    actualizo ejercicio

--- Ejercicios_logica/src/calculo/peso/CalculoPeso.java
+++ Ejercicios_logica/src/calculo/peso/CalculoPeso.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CalculoPeso {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in); 
 		System.out.println("Introduzca el valor del peso:");
 		double peso = Double.parseDouble(keyboard.nextLine()); //Double para admitir pesos con decimales
 		double pesoLUNA = peso*17/100 ; // 17% = 17/100 
